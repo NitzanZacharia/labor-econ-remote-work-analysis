341e2e2f59b5b38293291cb54a4a6cebb3e63ce4
Nitzan Zacharia <zacharia1@mail.tau.ac.il>
2026-06-07 19:14:02 +0300

    fixed MishlachYad filter

--- clean_data.R
+++ clean_data.R
@@ -14,22 +14,11 @@ dim(lfs_data_raw)
 # 2. ניקוי הנתונים ויצירת משתני ליבה
 clean_df <- lfs_data_raw %>%
   mutate(
-    # איחוד משתנים שהשם שלהם התפצל בין 2017 ל-2018 (באמצעות coalesce)
     SibaAvadPachot_Unified = coalesce(SibaAvadPachot, SibaAvadPachotmechushav),
     KamaPachot_Unified = coalesce(KamaPachot, KamaPachotmechushav),
-    
-    # הגדרת קבוצת הטיפול (אימהות): 1 אם יש לפחות ילד אחד מתחת לגיל 17, 0 אחרת
     Mother = ifelse(MisparYeladimAd17MB > 0, 1, 0),
-    
-    # הגדרת תקופת הזמן (Post): 1 לשנות הפוסט-קורונה, 0 לשנים שלפני
     Post = ifelse(ShnatSeker >= 2021, 1, 0),
-    
-    # הגדרת מועסקת (יש לבדוק בספר הצפנים ש-1 אכן מייצג "מועסק")
     Employed = ifelse(Muasak == 1, 1, 0),
-    
-    # יצירת משתנה עבודה מרחוק (WFH)
-    # משום שהשאלה נוספה רק ב-2020, נקודד אותה רק עבור 2021 ואילך. 
-    # עבור שנים קודמות נשאיר את הערך כחסר (NA) כדי שלא יעוות ממוצעים.
     WFH = case_when(
       ShnatSeker >= 2021 & AvodaMeHaBayit == 1 ~ 1,
       ShnatSeker >= 2021 & AvodaMeHaBayit != 1 ~ 0,
@@ -37,15 +26,22 @@ clean_df <- lfs_data_raw %>%
     )
   ) %>%
   
-  # סינון לאוכלוסיית היעד
+  # סינון נשים, גילאים ושנים (כפי שהגדרנו)
   filter(
-    Min == 2,                                               # נשים בלבד
+    Min == 2,                                               
     # Glink 3 = age group 25-29
     # Glink 7 = age group 55-59 (since 8 is 60-64 and some retire at 62 and some 65)
     GilNK >= 3 & GilNK <= 7,                               
-    ShnatSeker %in% c(2017, 2018, 2019, 2021, 2022, 2023)   # השמטת 2020
+    ShnatSeker %in% c(2017, 2018, 2019, 2021, 2022, 2023)   
   ) %>%
   
-  # סינון תצפיות שחסר להן סיווג מקצוע (לא ניתן לחשב עבורן את המדד)
-  drop_na(MishlachYad_ISCO_08_2)
+  # ------ התיקון שלנו ------
+# 1. סינון שורות שבהן סיווג המקצוע הוא "XX" (או NA אמיתי ליתר ביטחון)
+filter(
+  MishlachYad_ISCO_08_2 != "XX",
+  !is.na(MishlachYad_ISCO_08_2)
+) %>%
+  
+  # 2. המרת עמודת המקצוע מטקסט למספרים נקיים
+  mutate(MishlachYad_ISCO_08_2 = as.numeric(MishlachYad_ISCO_08_2)) %>%
 colnames(clean_df)
